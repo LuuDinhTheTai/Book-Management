ea76adc414726752bba80081e05236ce8eecb818
Luu Dinh The Tai <luudinthetai741@gmail.com>
2025-08-02 16:00:54 +0700

    refactor:

---
+++ src/main/java/com/me/book_management/dto/request/cart/ListCartRequest.java
@@ -0,0 +1,23 @@
+package com.me.book_management.dto.request.cart;
+
+import lombok.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class ListCartRequest {
+
+    private int page;
+    private int size;
+
+    public Pageable getPageable() {
+        int validPage = Math.max(1, page);
+        int validSize = Math.max(10, size);
+        return PageRequest.of(validPage - 1, validSize);
+    }
+} 
